fbce39a04f7c265c80af64f475ae0b2cf770d9c7
Travis Haas <travis@polleverywhere.com>
2024-09-13 15:32:06 -0400

    WIP: fixes

--- build_containers.sh
+++ build_containers.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-NGINX_TAG=docker-registry.ops.pe/streamer:nginx-latest
-STREAMER_TAG=docker-registry.ops.pe/streamer:streamer-latest
+NGINX_TAG=docker-registry.ops.pe/poll-streamer:nginx-latest
+STREAMER_TAG=docker-registry.ops.pe/poll-streamer:streamer-latest
 
 echo "Building nginx..."
-docker buildx build --platform linux/arm64 --file="./docker/nginx/Dockerfile" --tag=$NGINX_TAG ./docker/nginx
+docker buildx build --platform linux/arm64 --file="./docker/nginx/Dockerfile" --push --tag=$NGINX_TAG ./docker/nginx
 
 echo "Building streamer..."
-docker buildx build --platform linux/arm64 --file="./docker/streamer/Dockerfile" --tag=$STREAMER_TAG .
+docker buildx build --platform linux/arm64 --file="./docker/streamer/Dockerfile" --push --tag=$STREAMER_TAG .
 
 echo "All Containers built."
